071deafe1b0463c9f7037ba7fbd85d62fd011d66
cui yuchen <cuiyuchen@cuis-Air.hitronhub.home>
2022-06-27 21:18:11 -0400

    build schema

--- seeds/index.js
+++ seeds/index.js
@@ -5,9 +5,6 @@ const seedPaintings = require('./paintingData');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await seedGallery();
-
-  await seedPaintings();
 
   process.exit(0);
 };
